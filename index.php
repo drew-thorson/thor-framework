a34d999b1fc10d1950acf78e38eb26650d7eaf8b
Drew Thorson <thorsdre@gmail.com>
2016-01-21 14:34:00 -0600

    update gitignore

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+
+	<title>thor-framework</title>
+
+	<link rel="stylesheet" type="text/css" href="css/thor-framework.css" />
+
+</head>
+
+<body>
+
+	<div class="l-wrapper">
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+				<h1 class="sg-heading">Headings</h1>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+				<h1>Heading 1</h1>
+				<h2>Heading 2</h2>
+				<h3>Heading 3</h3>
+				<h4>Heading 4</h4>
+				<h5>Heading 5</h5>
+				<h6>Heading 6</h6>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+				<h1 class="sg-heading">Paragraphs</h1>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+				<p>Nunc quis malesuada erat, posuere suscipit urna. Integer ornare nisl vel turpis faucibus ullamcorper. Duis vel mollis leo, eu hendrerit risus. Cras ullamcorper volutpat pretium. Aliquam blandit mollis mauris sit amet luctus. Quisque vitae tincidunt libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+				<h1 class="sg-heading">Code Display</h1>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+<pre class="prettyprint">
+&lt;div class="l-row"&gt;
+	&lt;div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2"&gt;
+		&lt;button class="c-button c-button--primary c-button--full-width"&gt;Primary&lt;/button&gt;
+	&lt;/div&gt;
+	&lt;div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2"&gt;
+		&lt;button class="c-button c-button--primary c-button--full-width c-button--edge"&gt;Primary&lt;/button&gt;
+	&lt;/div&gt;
+	&lt;div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2"&gt;
+		&lt;button class="c-button c-button--primary c-button--full-width c-button--rounded"&gt;Primary&lt;/button&gt;
+	&lt;/div&gt;
+	&lt;div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2"&gt;
+		&lt;button class="c-button c-button--primary c-button--full-width c-button--pill"&gt;Primary&lt;/button&gt;
+	&lt;/div&gt;
+	&lt;div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2"&gt;
+		&lt;button class="c-button c-button--primary c-button--full-width c-button--default"&gt;Primary&lt;/button&gt;
+	&lt;/div&gt;
+	&lt;div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2"&gt;
+		&lt;button class="c-button c-button--primary c-button--full-width" disabled&gt;Primary&lt;/button&gt;
+	&lt;/div&gt;
+&lt;/div&gt;
+</pre>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-1">
+				<h1 class="sg-heading">Buttons</h1>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--primary c-button--full-width">Primary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--primary c-button--full-width c-button--edge">Primary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--primary c-button--full-width c-button--rounded">Primary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--primary c-button--full-width c-button--pill">Primary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--primary c-button--full-width c-button--default">Primary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--primary c-button--full-width" disabled>Primary</button>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--secondary c-button--full-width">Secondary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--secondary c-button--full-width c-button--edge">Secondary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--secondary c-button--full-width c-button--rounded">Secondary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--secondary c-button--full-width c-button--pill">Secondary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--secondary c-button--full-width c-button--default">Secondary</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--secondary c-button--full-width" disabled>Secondary</button>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--success c-button--full-width">Success</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--success c-button--full-width c-button--edge">Success</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--success c-button--full-width c-button--rounded">Success</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--success c-button--full-width c-button--pill">Success</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--success c-button--full-width c-button--default">Success</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--success c-button--full-width" disabled>Success</button>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--danger c-button--full-width">Danger</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--danger c-button--full-width c-button--edge">Danger</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--danger c-button--full-width c-button--rounded">Danger</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--danger c-button--full-width c-button--pill">Danger</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--danger c-button--full-width c-button--default">Danger</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--danger c-button--full-width" disabled>Danger</button>
+			</div>
+		</div>
+		<div class="l-row">
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--warning c-button--full-width">Warning</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--warning c-button--full-width c-button--edge">Warning</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--warning c-button--full-width c-button--rounded">Warning</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--warning c-button--full-width c-button--pill">Warning</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--warning c-button--full-width c-button--default">Warning</button>
+			</div>
+			<div class="l-lg-col-1-6 l-med-col-1-3 l-sml-1-2">
+				<button class="c-button c-button--warning c-button--full-width" disabled>Warning</button>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
